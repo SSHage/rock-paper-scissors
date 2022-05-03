b582550383eeca933cc29479acb9e2cf7b1cc7f3
Seah Siang Hwee <sianghwee@Seahs-MacBook-Pro.local>
2022-05-03 23:56:15 +0800

    added button functionality. To be added: DOM messages and cleanup of site.

--- javascript.js
+++ javascript.js
@@ -1,25 +1,29 @@
-let playerWinCount = 0, computerWinCount = 0;
+let playerWinCount = 0, computerWinCount = 0 , roundNumber=1;
 
-let game = () => {
-    let i;
+let game = (playerSelection) => {
+    /*let i;
     //play till one person reaches score of 5
     for (i=1; ; i++) {
         if (playerWinCount==5 || computerWinCount==5){
             break;
         }
-        playRound(i);
-    }
+        playRound(playerSelection, i);
+    }*/
 
     if (playerWinCount==5){
         console.log("Final Score - Player: %i. Computer: %i. Player wins!", playerWinCount, computerWinCount);
     }
-    else {
+    else if (computerWinCount==5) {
         console.log("Final Score - Player: %i. Computer: %i. Computer wins!", playerWinCount, computerWinCount);       
     }
 
+    else{
+        playRound(playerSelection,roundNumber);
+        roundNumber++;
+    }
 }
 
-let playRound = (roundNumber) => {
+let playRound = (playerSelection, roundNumber) => {
 
     // generate number from 1-3 to randomise Rock/Paper/Scissors //
     let randomNumber = Math.floor(Math.random()*3)+1;
@@ -31,50 +35,61 @@ let playRound = (roundNumber) => {
     "Paper" : "Scissors";
 
     // get player input //
-    playerSelection = prompt("Enter your selection. (Rock/Paper/Scissors)");
+    //playerSelection = prompt("Enter your selection. (Rock/Paper/Scissors)");
     playerSelection = playerSelection[0].toUpperCase() + playerSelection.slice(1).toLowerCase();
 
-if (computerSelection=="Rock"){
-    if (playerSelection=="Paper"){
-        playerWinCount++;
-        console.log("Round %i - You win! %s beats %s.", roundNumber, playerSelection, computerSelection);
-    }
-    else if (playerSelection=="Scissors"){
-        computerWinCount++;
-        console.log("Round %i - You lose! %s loses to %s.", roundNumber, playerSelection, computerSelection);
-    }
-    else {
-        console.log("Round %i - It's a draw!", roundNumber);
-    }
-} 
+    if (computerSelection=="Rock"){
+        if (playerSelection=="Paper"){
+            playerWinCount++;
+            console.log("Round %i - You win! %s beats %s.", roundNumber, playerSelection, computerSelection);
+        }
+        else if (playerSelection=="Scissors"){
+            computerWinCount++;
+            console.log("Round %i - You lose! %s loses to %s.", roundNumber, playerSelection, computerSelection);
+        }
+        else {
+            console.log("Round %i - It's a draw!", roundNumber);
+        }
+    } 
 
-else if (computerSelection=="Paper"){
-    if (playerSelection=="Scissors"){
-        playerWinCount++;
-        console.log("Round %i - You win! %s beats %s.", roundNumber, playerSelection, computerSelection);
+    else if (computerSelection=="Paper"){
+        if (playerSelection=="Scissors"){
+            playerWinCount++;
+            console.log("Round %i - You win! %s beats %s.", roundNumber, playerSelection, computerSelection);
+        }
+        else if (playerSelection=="Rock"){
+            computerWinCount++;
+            console.log("Round %i - You lose! %s loses to %s.", roundNumber, playerSelection, computerSelection);
+        }
+        else {
+            console.log("Round %i - It's a draw!", roundNumber);
+        }
     }
-    else if (playerSelection=="Rock"){
-        computerWinCount++;
-        console.log("Round %i - You lose! %s loses to %s.", roundNumber, playerSelection, computerSelection);
-    }
-    else {
-        console.log("Round %i - It's a draw!", roundNumber);
+
+    else if (computerSelection=="Scissors"){
+        if (playerSelection=="Rock"){
+            playerWinCount++;
+            console.log("Round %i - You win! %s beats %s.", roundNumber, playerSelection, computerSelection);
+        }
+        else if (playerSelection=="Paper"){
+            computerWinCount++;
+            console.log("Round %i - You lose! %s loses to %s.", roundNumber, playerSelection, computerSelection);
+        }
+        else {
+            console.log("Round %i - It's a draw!", roundNumber);
+        }
     }
 }
 
-else if (computerSelection=="Scissors"){
-    if (playerSelection=="Rock"){
-        playerWinCount++;
-        console.log("Round %i - You win! %s beats %s.", roundNumber, playerSelection, computerSelection);
-    }
-    else if (playerSelection=="Paper"){
-        computerWinCount++;
-        console.log("Round %i - You lose! %s loses to %s.", roundNumber, playerSelection, computerSelection);
-    }
-    else {
-        console.log("Round %i - It's a draw!", roundNumber);
-    }
-}
+// initialise variables
+const selections = document.querySelectorAll('.selection');
+let playerInput;
 
+// add event listeners to all buttons. get unique ID of selected choice to launch game
+for (const selection of selections) {
+    selection.addEventListener('click', function(e){
+        playerInput = this.getAttribute('id');
+        game(playerInput);
+    });
+  }
 
-}
